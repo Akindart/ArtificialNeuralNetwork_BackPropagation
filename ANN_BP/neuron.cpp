ef23f8917fc4993b4e29e23f3d07fe587a07f85a
helderc <heldercro@gmail.com>
2013-06-05 08:59:49 -0300

    Funcoes e Interface

--- ANN_BP/neuron.cpp
+++ ANN_BP/neuron.cpp
@@ -4,3 +4,23 @@ Neuron::Neuron(QObject *parent) :
     QObject(parent)
 {
 }
+
+double Neuron::calcTangHiperbolic(double n)
+{
+    return ((1 - exp(-2*n)) / (1 + exp(-2*n)));
+}
+
+double Neuron::calcDerivTangHiperbolic(double n)
+{
+    return (1 - (calcTangHiperbolic(n) * calcTangHiperbolic(n)));
+}
+
+double Neuron::calcLogistic(double n)
+{
+    return (1 / (1 + exp(-n)));
+}
+
+double Neuron::calcDerivLogistic(double n)
+{
+    return ( exp(-n) / ((1 + exp(-n)) * (1 + exp(-n))) );
+}
--- ANN_BP/neuron.h
+++ ANN_BP/neuron.h
@@ -3,11 +3,19 @@
 
 #include <QObject>
 
+#include <cmath>
+
 class Neuron : public QObject
 {
     Q_OBJECT
 public:
     explicit Neuron(QObject *parent = 0);
+
+private:
+    double calcTangHiperbolic(double n);
+    double calcDerivTangHiperbolic(double n);
+    double calcLogistic(double n);
+    double calcDerivLogistic(double n);
 
 signals:
 
